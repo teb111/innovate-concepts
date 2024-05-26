6d678853434b9f39d572046ef16510811a4c4106
Oluwa Tobiloba <fakoredetobiloba.ft@gmail.com>
2024-05-26 17:45:21 +0100

    mmmm2

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,96 +1,96 @@
-import React, { useState } from 'react'
-import Header from '../components/Header'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Textarea from '@mui/joy/Textarea'
-import { Button } from '@mui/material'
-import { sendMail } from '../utils'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Textarea from "@mui/joy/Textarea";
+import { Button } from "@mui/material";
+import { sendMail } from "../utils";
 
 const ContactScreen = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [subject, setSubject] = useState('')
-  const [message, setMessage] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
   const submitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const reqBody = {
       name,
       email,
       subject,
-      message
-    }
-    sendMail(reqBody)
-  }
+      message,
+    };
+    sendMail(reqBody);
+  };
   return (
-    <>
+    <div className="page-container">
       <Header />
       <h2>Contact</h2>
-      <div className='contact-container'>
-        <div className='contact-info'>
+      <div className="contact-container">
+        <div className="contact-info">
           <h3>Company Contact Information</h3>
 
           <h4>Lagos</h4>
-          <p className='text-wrap'>
+          <p className="text-wrap">
             <p>33a Jay Jay Oladimeji Close,</p>
             <p> Lagos, Nigeria</p>
             <p>
               <span>Phone </span>: +234 818 582 8965
             </p>
             <p>
-              {' '}
-              <span>Email:</span>{' '}
-              <a href='mailto:info@innovateconceptsltd.com'>
+              {" "}
+              <span>Email:</span>{" "}
+              <a href="mailto:info@innovateconceptsltd.com">
                 info@innovateconceptsltd.com
               </a>
             </p>
             <p>
-              {' '}
-              <span>Email:</span>{' '}
-              <a href='mailto:innovatearchitects@yahoo.com'>
+              {" "}
+              <span>Email:</span>{" "}
+              <a href="mailto:innovatearchitects@yahoo.com">
                 innovatearchitects@yahoo.com
               </a>
             </p>
           </p>
         </div>
-        <div className='map-email'>
+        <div className="map-email">
           <iframe
-            src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Freedom%20Way%20Eti-Osa%20Lagos%20Nigeria+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
-            className='map-frame'
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Freedom%20Way%20Eti-Osa%20Lagos%20Nigeria+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            className="map-frame"
             style={{ border: 0 }}
-            allowfullscreen=''
-            loading='lazy'
-            referrerpolicy='no-referrer-when-downgrade'
-            title='map'
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="map"
           ></iframe>
-          <div className='form-wrapper'>
+          <div className="form-wrapper">
             <h4>Send us an email</h4>
             <p>
               Please tell us about your business. Innovate concepts
               representative will walk you through our services
             </p>
             <Box
-              component='form'
+              component="form"
               sx={{
-                '& .MuiTextField-root': { m: 1, width: '100%' }
+                "& .MuiTextField-root": { m: 1, width: "100%" },
               }}
               noValidate
-              autoComplete='off'
+              autoComplete="off"
             >
               <div>
                 <TextField
                   required
-                  id='outlined-required'
-                  label='Name'
-                  placeholder='Enter Your name'
+                  id="outlined-required"
+                  label="Name"
+                  placeholder="Enter Your name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
                 <TextField
                   required
-                  id='outlined-required'
-                  label='Email'
-                  placeholder='Enter Your email address'
+                  id="outlined-required"
+                  label="Email"
+                  placeholder="Enter Your email address"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
@@ -102,15 +102,15 @@ const ContactScreen = () => {
                 /> */}
                 <TextField
                   required
-                  id='outlined-required'
-                  label='Subject'
-                  placeholder='Please enter Email Subject'
+                  id="outlined-required"
+                  label="Subject"
+                  placeholder="Please enter Email Subject"
                   value={subject}
                   onChange={(e) => setSubject(e.target.value)}
                 />
                 <Textarea
                   minRows={2}
-                  placeholder='Type Message…'
+                  placeholder="Type Message…"
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                 />
@@ -118,13 +118,13 @@ const ContactScreen = () => {
             </Box>
 
             <Button
-              variant='contained'
-              color='primary'
+              variant="contained"
+              color="primary"
               style={{
-                marginTop: '20px',
-                display: 'flex',
-                justifyContent: 'flex-end',
-                padding: '10px'
+                marginTop: "20px",
+                display: "flex",
+                justifyContent: "flex-end",
+                padding: "10px",
               }}
               onClick={submitHandler}
             >
@@ -133,8 +133,8 @@ const ContactScreen = () => {
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default ContactScreen
+export default ContactScreen;
--- src/screens/ServiceScreen.js
+++ src/screens/ServiceScreen.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import Header from '../components/Header'
-import Tabs from '@mui/material/Tabs'
-import Tab from '@mui/material/Tab'
+import React from "react";
+import Header from "../components/Header";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
-import { CheckOutlined } from '@mui/icons-material'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import ArrowRightIcon from '@mui/icons-material/ArrowRight'
+import { CheckOutlined } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 const ServiceScreen = () => {
-  const [value, setValue] = React.useState(0)
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
   function a11yProps(index) {
     return {
       id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`
-    }
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
   }
 
   function TabPanel(props) {
-    const { children, value, index, ...other } = props
+    const { children, value, index, ...other } = props;
 
     return (
       <div
-        role='tabpanel'
+        role="tabpanel"
         hidden={value !== index}
         id={`simple-tabpanel-${index}`}
         aria-labelledby={`simple-tab-${index}`}
@@ -38,262 +38,302 @@ const ServiceScreen = () => {
           </Box>
         )}
       </div>
-    )
+    );
   }
 
   return (
     <>
-      <Header />
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        // aria-label='icon label tabs example'
-        className='tabs-style'
-      >
-        <Tab
-          icon={<CheckOutlined sx={{ fontSize: 40 }} />}
-          label='Architecture'
-        />
-        <Tab
-          icon={<CheckOutlined sx={{ fontSize: 40 }} />}
-          label='Engineering'
-        />
-        <Tab
-          icon={<CheckOutlined sx={{ fontSize: 40 }} />}
-          label='Project Management'
-        />
-        <Tab
-          icon={<CheckOutlined sx={{ fontSize: 40 }} />}
-          label='Construction'
-          {...a11yProps(0)}
-        />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <div className='content-body'>
-          <div>
-            <h2>Architecture</h2>
-            <div className='seperator'></div>
+      <div className="page-container">
+        <Header />
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          // aria-label='icon label tabs example'
+          className="tabs-style"
+        >
+          <Tab
+            icon={<CheckOutlined sx={{ fontSize: 40 }} />}
+            label="Architecture"
+          />
+          <Tab
+            icon={<CheckOutlined sx={{ fontSize: 40 }} />}
+            label="Engineering"
+          />
+          <Tab
+            icon={<CheckOutlined sx={{ fontSize: 40 }} />}
+            label="Project Management"
+          />
+          <Tab
+            icon={<CheckOutlined sx={{ fontSize: 40 }} />}
+            label="Construction"
+            {...a11yProps(0)}
+          />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <div className="mt-4">
+            <img
+              src={
+                "https://res.cloudinary.com/teb/image/upload/v1716741190/Architecture_x7upbc.jpg"
+              }
+              className="w-100 services-img"
+              alt="work"
+            />
           </div>
-
-          <div className='content-inner'>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Brief Formulation
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Feasibility Studies
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Concept Design
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Preliminary Drawing
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Production Information
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Contract Administration and Supervision
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Project Handover and Closeout
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Post Occupancy Evaluation
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <div className='content-body'>
-          <div>
-            <h2>Engineering</h2>
-            <div className='seperator'></div>
-          </div>
-
-          <div className='content-inner'>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Power Distribution System Design
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Power Quality Testing & Monitoring
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Systems Commissioning
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Lighting Design
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Plumbing (Water Supply System, Public Health, Rainwater
-                  Drainage and Suppression)
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Borehole and Water Treatment Systems
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Sewage Treatment Systems
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Swimming Pools, Fountains & Water Display Features
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  HVAC design
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Fire Protection & Suppression Systems
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Escalator, Elevator & Travelator
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Concept Design
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Detailed Engineering Design(Concrete and Steel structures)
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Construction supervision{' '}
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Evaluation of structural integrity of defective buildings
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Highway and drainage designs
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Fire Detection and Alarm Systems
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Burglar Alarms
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  CCTV
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Access Control
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Voice & Data Systems
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <div className='content-body'>
-          <div>
-            <h2>Project Management</h2>
-            <div className='seperator'></div>
-          </div>
-
-          <div className='content-inner'>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Project Feasibility & Inception
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Project Guidance & Pre-Contract
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Concept Design
-                </li>
-              </ul>
+          <div className="content-body">
+            <div>
+              <h2>Architecture</h2>
+              <div className="seperator"></div>
             </div>
 
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Value Engineering & Analysis
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Cost Control & Content Analysis + Recommendations
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  Quality Control and Monitoring & Control of Construction
-                  Processes
-                </li>
-              </ul>
-            </div>
-            <div className='content-list'>
-              <ul>
-                <li>
-                  <ArrowRightIcon />
-                  Content Framework
-                </li>
-                <li>
-                  <ArrowRightIcon />
-                  HSE Awareness
-                </li>
-              </ul>
+            <div className="content-inner">
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Brief Formulation
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Feasibility Studies
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Concept Design
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Preliminary Drawing
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Production Information
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Contract Administration and Supervision
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Project Handover and Closeout
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Post Occupancy Evaluation
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </TabPanel>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <div className="mt-4">
+            <img
+              src={
+                "https://res.cloudinary.com/teb/image/upload/v1716741189/Engineering_lit3do.jpg"
+              }
+              className="w-100 services-img"
+              alt="work"
+            />
+          </div>
+          <div className="content-body">
+            <div>
+              <h2>Engineering</h2>
+              <div className="seperator"></div>
+            </div>
+
+            <div className="content-inner">
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Power Distribution System Design
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Power Quality Testing & Monitoring
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Systems Commissioning
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Lighting Design
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Plumbing (Water Supply System, Public Health, Rainwater
+                    Drainage and Suppression)
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Borehole and Water Treatment Systems
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Sewage Treatment Systems
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Swimming Pools, Fountains & Water Display Features
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    HVAC design
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Fire Protection & Suppression Systems
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Escalator, Elevator & Travelator
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Concept Design
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Detailed Engineering Design(Concrete and Steel structures)
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Construction supervision{" "}
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Evaluation of structural integrity of defective buildings
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Highway and drainage designs
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Fire Detection and Alarm Systems
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Burglar Alarms
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    CCTV
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Access Control
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Voice & Data Systems
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <div className="mt-4">
+            <img
+              src={
+                "https://res.cloudinary.com/teb/image/upload/v1716741191/Project_mgt_llp1yp.jpg"
+              }
+              className="w-100 services-img"
+              alt="work"
+            />
+          </div>
+          <div className="content-body">
+            <div>
+              <h2>Project Management</h2>
+              <div className="seperator"></div>
+            </div>
+
+            <div className="content-inner">
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Project Feasibility & Inception
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Project Guidance & Pre-Contract
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Concept Design
+                  </li>
+                </ul>
+              </div>
+
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Value Engineering & Analysis
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Cost Control & Content Analysis + Recommendations
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    Quality Control and Monitoring & Control of Construction
+                    Processes
+                  </li>
+                </ul>
+              </div>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <ArrowRightIcon />
+                    Content Framework
+                  </li>
+                  <li>
+                    <ArrowRightIcon />
+                    HSE Awareness
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <div className="mt-4">
+            <img
+              src={
+                "https://res.cloudinary.com/teb/image/upload/v1716741198/Construction_wgk6vg.jpg"
+              }
+              className="w-100 services-img"
+              alt="work"
+            />
+          </div>
+        </TabPanel>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ServiceScreen
+export default ServiceScreen;
